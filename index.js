8b08b62db94bb1145b4629424e96428776dcaecc
ruihildt <ruihildt@protonmail.com>
2019-07-11 11:20:51 +0200

    Remove comment

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const server = require('./api/server.js');
 
-// Haha
-const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
   console.log(`*** Server Running on http://localhost:${port} ***`);
