01db364aa91d73e1e6c9a6da2fce5bfdd7469a81
ruihildt <ruihildt@protonmail.com>
2019-07-11 11:45:47 +0200

    Add .env file for port and API key

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 const server = require('./api/server.js');
 
+const port = process.env.PORT;
+const key = process.env.API_KEY;
 
 server.listen(port, () => {
-  console.log(`*** Server Running on http://localhost:${port} ***`);
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n);
 });
